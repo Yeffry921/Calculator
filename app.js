25d50ea53128ac837847f26f6cec3648cc7485a0
yeffry <yeffry45@gmail.com>
2020-04-09 21:28:39 -0400

    replaced calculator logic with a class

--- app.js
+++ app.js
@@ -1,87 +1,242 @@
-const add = (num1, num2) => num1 + num2;
-
-const subtract = (num1, num2) => num1 - num2;
-
-const multiply = (num1, num2) => num1 * num2;
-
-const divide = (num1, num2) =>{
-    if(num1 === 0|| num2 === 0){
-        return 'Error, can\'t divide by zero';
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        //run this as soon as it start, otherwise line 19 is undefined
+        this.allClear();
     }
-    return num1 / num2;
-} 
 
-const operation = (operand, num1, num2) => {
-    num1 = parseInt(num1);
-    num2 = parseInt(num2)
-    if (operand === '+') {
-        return add(num1, num2)
-    } else if (operand === '-') {
-        return subtract(num1, num2)
-    } else if (operand === '*') {
-        return multiply(num1, num2)
-    } else if (operand === '/') {
-        return divide(num1, num2)
-    }
-};
-
-const clearDisplay = (storeVal, displayValue) => {
-    storeVal.first = '';
-    storeVal.operand = '';
-    storeVal.second = '';
-    storeVal.result = '';
-    storeVal.decimal = '';
-    displayValue.value = '';
-};
-
-const multipleOperands = (storeVal, e) => {
-    storeVal.result = operation(storeVal.operand, storeVal.first, storeVal.second);
-    storeVal.first = storeVal.result;
-    storeVal.operand = e.target.value;
-    storeVal.second = '';
-};
-
-let displayValue = document.querySelector('.output');
-displayValue.value = '';
-
-const storeVal = {
-    first: '',
-    second: '',
-    operand: '',
-    result: '',
-    decimal: ''
-};
-
-document.querySelectorAll('button').forEach((number) => {
-    number.addEventListener('click', (e) => {
-
-        //store the first number to be operated when there is no operand yet
-        if (e.target.classList.contains('number') && storeVal.operand === '') {
-            storeVal.first += e.target.value;
-            displayValue.value = storeVal.first;
+    grabValues(number) {
+        if(this.currentValue.length <= 7){
+            this.currentValue = this.currentValue + number
         }
-
-        if (e.target.classList.contains('operand')) { 
-            if (storeVal.first && storeVal.operand && storeVal.second) {
-                multipleOperands(storeVal, e);
-                displayValue.value = storeVal.result + storeVal.operand;
-
-            } else {
-                storeVal.operand = e.target.value;
-                displayValue.value = storeVal.first + storeVal.operand;
-
-            }
-        //store the second number when there is an operand
-        } else if (e.target.classList.contains('number') && storeVal.operand !== '') { 
-            storeVal.second += e.target.value;
-            displayValue.value = storeVal.first + storeVal.operand + storeVal.second;
-
-        } else if (e.target.value === '=' && storeVal.first && storeVal.operand && storeVal.second) {
-            displayValue.value = operation(storeVal.operand, storeVal.first, storeVal.second);
-
-        } else if (e.target.value === 'C') {
-            clearDisplay(storeVal, displayValue);
-
+        return;
+    }
+    grabOperand(operand) {
+        this.operand = operand;
+        this.previousValue = this.currentValue;
+        this.currentValue = '';
+    }
+    allClear() {
+        this.currentValue = '';
+        this.previousValue = '';
+        this.operand = '';
+    }
+    delete() {
+        this.currentValue = this.currentValue.toString().slice(0, -1)
+    }
+    compute() {
+        const prev = parseFloat(this.previousValue);
+        const curr = parseFloat(this.currentValue);
+        let result;
+        if(this.operand === '+'){
+            result = prev + curr
+        } else if(this.operand === '-'){
+            result = prev - curr
+        } else if(this.operand === '*'){
+            result = prev * curr
+        } else if(this.operand === '÷'){
+            result = prev / curr
         } 
+
+        if(result.toString().length > 8){
+            this.currentValue = 'Error';
+            return;
+        }
+        this.currentValue = result;
+        this.operand = '';
+        this.previousValue = ''
+
+    }
+    updateUI(){
+        this.currentOperandTextElement.textContent = this.currentValue;
+        this.previousOperandTextElement.textContent = `${this.previousValue} ${this.operand}`;
+    }
+};
+
+const numberBtns = document.querySelectorAll('[data-number]');
+const operationBtns = document.querySelectorAll('[data-operation]');
+const equalsBtn = document.querySelector('[data-equals]');
+const deleteBtn = document.querySelector('[data-delete]');
+const allClearBtn = document.querySelector('[data-all-clear]');
+const currentOperandTextElement = document.querySelector('[data-previous-operand]');
+const previousOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+
+numberBtns.forEach((number) => {
+    number.addEventListener('click', () => {
+        calculator.grabValues(number.textContent);
+        calculator.updateUI();
     })
 });
+
+operationBtns.forEach((operand) => {
+    operand.addEventListener('click', () => {
+        calculator.grabOperand(operand.textContent);
+        calculator.updateUI();
+    })
+});
+
+equalsBtn.addEventListener('click', () => {
+    calculator.compute();
+    calculator.updateUI();
+});
+
+deleteBtn.addEventListener('click', () => {
+    calculator.delete();
+    calculator.updateUI();
+});
+
+allClearBtn.addEventListener('click', () => {
+    calculator.allClear();
+    calculator.updateUI();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Calculator {
+//     constructor(previousOperandTextElement, currentOperandTextElement) {
+//         this.previousOperandTextElement = previousOperandTextElement;
+//         this.currentOperandTextElement = currentOperandTextElement;
+//         this.allClear();
+//     }
+//     allClear() {
+//         this.currentOperand = '';
+//         this.previousOperand = '';
+//         this.operation = undefined;
+//     }
+//     delete() {
+//         this.currentOperand = this.currentOperand.toString().slice(0, -1);
+//     }
+//     appendNumber(number) {
+//         if (this.currentOperand.includes('.') && number === '.') return;
+//         this.currentOperand = this.currentOperand.toString() + number.toString();
+//     }
+//     chooseOperation(operation) {
+//         if (this.currentOperand === '') return;
+//         if (this.previousOperand !== '') {
+//             this.compute();
+//         }
+//         this.operation = operation;
+//         this.previousOperand = this.currentOperand;
+//         this.currentOperand = '';
+//     }
+//     compute() {
+//         let computation;
+//         const prev = parseFloat(this.previousOperand);
+//         const current = parseFloat(this.currentOperand);
+//         if (isNaN(prev) || isNaN(current)) return
+
+//         switch (this.operation) {
+//             case '+':
+//                 computation = prev + current;
+//                 break;
+//             case '-':
+//                 computation = prev - current;
+//                 break;
+
+//             case '*':
+//                 computation = prev * current;
+//                 break;
+
+//             case '÷':
+//                 computation = prev / current;
+//                 break;
+//             default:
+//                 return
+//         }
+//         this.currentOperand = computation
+//         this.operation = undefined
+//         this.previousOperand = ''
+
+//     }
+//     updateDisplay() {
+//         this.currentOperandTextElement.textContent = this.getDisplayNumber(this.currentOperand);
+//         if(this.operation != null) {
+//             this.previousOperandTextElement.textContent = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+//         }
+//     }
+// }
+
+// const numberBtns = document.querySelectorAll('[data-number]');
+// const operationBtns = document.querySelectorAll('[data-operation]');
+// const equalsBtn = document.querySelector('[data-equals]');
+// const deleteBtn = document.querySelector('[data-delete]');
+// const allClearBtn = document.querySelector('[data-all-clear]');
+// const currentOperandTextElement = document.querySelector('[data-previous-operand]');
+// const previousOperandTextElement = document.querySelector('[data-current-operand]');
+
+// const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+// numberBtns.forEach((button) => {
+//     button.addEventListener('click', () => {
+//         calculator.appendNumber(button.innerText);
+//         calculator.updateDisplay();
+//     })
+// });
+
+// operationBtns.forEach((operation) => {
+//     operation.addEventListener('click', () => {
+//         calculator.chooseOperation(operation.textContent);
+//         calculator.updateDisplay();
+//     })
+// });
+
+// equalsBtn.addEventListener('click', () => {
+//     calculator.compute();
+//     calculator.updateDisplay();
+// });
+
+// deleteBtn.addEventListener('click', () => {
+//     calculator.delete();
+//     calculator.updateDisplay();
+// });
+
+// allClearBtn.addEventListener('click', () => {
+//     calculator.allClear();
+//     calculator.updateDisplay();
+// });
